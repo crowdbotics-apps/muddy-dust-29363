1a8257364de12ee706bd8b1e94ee1f340e06a3bb
Crowdbotics <team@crowdbotics.com>
2021-08-03 01:15:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "muddy_dust_29363Identifier",
+  url: "https://muddy_dust_29363Identifier.botics.co",
+  api: "https://muddy_dust_29363Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
